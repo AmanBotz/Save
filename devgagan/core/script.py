8753ab5bf7d4c1e780d2aedfd1bbff780ca3694f
AmanBotz <amanpooniaff@gmail.com>
2024-12-30 17:21:18 +0530

    Add files via upload

---
+++ devgagan/core/script.py
@@ -0,0 +1,83 @@
+#amanbots
+
+# ------------------------------------------------------------ #
+
+START_TXT = """
+Hi, welcome to Advance Content Saver Bot, designed to save restricted messages from public/private channels and private groups. First login in bot by /login then send post link.
+"""
+
+FORCE_MSG = """
+Hey {},
+
+According to my database, you've not joined the updates channel yet. If you want to use me, then join the updates channel and start me again!
+"""
+
+HELP_TXT = """
+HELP SECTION 📝
+
+🛠️ /settings - Open settings to set your requirements.
+
+🔒 /login - Login to your userbot session.
+
+📦 /batch - Download bulk links in a systematic way.
+
+⛔ /cancel - Stop batch processing.
+"""
+
+HELP2_TXT = """
+🕵️ Help:
+
+FOR PUBLIC AND PRIVATE CHANNEL OR GROUP:
+- First, log in.
+- Then send the message link of any channel that you've joined in your login account.
+
+FOR BOT:
+- Send the link in this format: https://t.me/b/bot_username/message_id (use Plus Messenger for message_id)
+
+FOR GROUP TOPIC:
+- (For Private Group) Group topic link is like: https://t.me/c/xxxxxxxxx/first_id/second_id
+But, send it like this: https://t.me/c/xxxxxxx/second_id (remove first id and one /)
+- (For Public Group) Follow the private link step but remove "/c" from the link. Ex - https://t.me/username/second_id
+
+#FAQ:
+
+- If the bot says "Have you joined the channel?" then just log in again to the bot and try.
+
+- If your batch is stuck, then use /stop.
+"""
+
+ADMIN_TXT = """
+ADMINS PANEL 🛠️
+
+➕ /add - Add user ID to the premium section.
+
+➖ /rem - Remove user ID from the premium section.
+
+🔍 /check - Check if a user ID is in the premium section.
+
+📢 /broadcast - Broadcast a message without a forward tag.
+
+📣 /announce - Broadcast a message with a forward tag.
+
+📊 /stats - Check your bot's stats.
+"""
+
+SETTINGS_TXT = """
+Welcome to the settings section. Here, you can choose button: caption or session and thumbnail.
+"""
+
+CAPTI0NS_TXT = """
+Customize the bot's caption here to tailor it to your preferences and needs!
+"""
+
+THUMBNAIL_TXT = """
+Customize the bot's thumbnail here to tailor it to your preferences and needs!
+"""
+
+SESSION_TXT = """
+Customize the bot's session here to tailor it to your preferences and needs!
+"""
+
+CHANNEL_TXT = """
+Customize the bot's channel here to tailor it to your preferences and needs!
+"""
